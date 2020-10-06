5317730b9ca7fd8302086638b5bb620fddd3c153
Mohammad Manjurul Islam <manjurulimd@gmail.com>
2020-10-06 17:04:51 +0200

    relational data using jdbc is corrected and fixed

--- relational-data-access/src/test/java/com/example/relationaldataaccess/RelationalDataAccessApplicationTests.java
+++ relational-data-access/src/test/java/com/example/relationaldataaccess/RelationalDataAccessApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.relationaldataaccess;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class RelationalDataAccessApplicationTests {
 
 	@Test
 	void contextLoads() {
